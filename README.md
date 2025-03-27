d057b32adc8767a5c66f207431f5dc17b026cf07
brubruh <maxim.van-dorn@uvm.edu>
2025-03-26 20:49:16 -0400

    Changed length of the paths to a little shorter.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Change Log:
 03/26/2025
 Max Van Dorn
 Testing push/pull to github through VSC.
+
+Changed length of the paths to a little shorter.
--- nodes.js
+++ nodes.js
@@ -47,7 +47,7 @@ d3.csv("data/nodes2.0.csv").then(function(data) {
 
   // Initialize the simulation with forces
   const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id).distance(600))
+    .force("link", d3.forceLink(links).id(d => d.id).distance(450))
     .force("charge", d3.forceManyBody().strength(-100))
     .force("center", d3.forceCenter(width / 2 + 100, height / 2));
 
